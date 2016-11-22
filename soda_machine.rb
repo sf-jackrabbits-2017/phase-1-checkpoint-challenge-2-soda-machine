d06f842eca5193ce83b56e9e5d3c46d3337a9413
valeriamartinez94 <valeriamartinez94@gmail.com>
2016-11-22 10:06:14 -0800

    Add method that finds soda, adds soda price to cash, and deletes soda from sodas

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,9 +1,10 @@
 class SodaMachine
-  attr_reader :sodas, :cash
+  attr_reader :sodas, :cash, :sold_soda
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    # @sold_soda = []
   end
 
   def current_inventory_count
@@ -16,7 +17,19 @@ class SodaMachine
   end
 
   def sell(soda_brand)
-
+    # p "SODA BRAND () ---------"
+    # p soda_brand
+    # p "FIND SODA--------------"
+    # found_it = find_soda(soda_brand)
+    # p found_it
+    found_it = find_soda(soda_brand)
+    if found_it
+      @sold_soda = found_it
+      @cash += @sold_soda.price
+      @sodas.delete(@sold_soda)
+    # p "SOLD SODA--------------"
+    # p @sold_soda
+    end
   end
 
 end
