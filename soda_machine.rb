18c38514043054c0a3d3c0c9eb8e5e7fe1f83585
yaritza perez <yaritzape9@gmail.com>
2016-11-22 10:06:39 -0800

    adds some methods for soda

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,9 +7,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    count = 0
+    @sodas.each do |sub_array|
+    sub_array.each_with_index do |item|
+      count += 1
+      puts item
+    end
+end
+
   end
 
   def find_soda(soda_brand)
+    @sodas[soda_brand]
   end
 
   def sell(soda_brand)
