8801093812b2ca3885a20144a5dba3fb1eb8b063
Amber Nash <nash.beram@gmail.com>
2016-11-22 10:06:29 -0800

    annnash88 checkpoint-challenge-wk2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+     @sodas.length
   end
 
   def find_soda(soda_brand)
+      result = @sodas.select!{|soda| soda == soda_brand }.first
+      result
   end
 
   def sell(soda_brand)
+    if find_soda
+      #sell a soda with given brand
+      #remove soda from sodas hash
+      @sodas.delete(soda_brand)
+      #price of soda add to cash hash
+      @cash << #@sodas.price
+    end
+
   end
 
 end
