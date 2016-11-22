d924e048df02cfc41fa2d7ad188ba9630a7eac71
Cllie Tomlinson <callielynne3@gmail.com>
2016-11-22 10:06:17 -0800

    Get rpsec tests to pass for machine methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,18 +7,21 @@ class SodaMachine
   end
 
   def current_inventory_count
-    # A soda machine will be able to return the number of sodas it contains. This method should be called SodaMachine#current_inventory_count.
+    # A soda machine will be able to return the number of sodas it contains.
     @sodas.length
   end
 
   def find_soda(soda_brand)
-    # A soda machine will be able to find a single soda with a given brand. This method will be called SodaMachine#find_soda.
+    # A soda machine will be able to find a single soda with a given brand.
     @sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
-    # A soda machine will be able to sell a soda with a given brand. When a soda is sold, it should be removed from the soda machine and the price of the soda should be added to the soda machine's cash This method will be called SodaMachine#sell.
-   
+    # A soda machine will be able to sell a soda with a given brand. 
+   sell_soda = find_soda(soda_brand)
+   return nil if sell_soda == nil
+   # When a soda is sold, it should be removed from the soda machine and the price of the soda should be added to the  soda machine's cash.
+   @cash += 
   end
 
 end
