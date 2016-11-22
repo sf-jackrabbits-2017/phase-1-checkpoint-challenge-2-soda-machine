743e60f05432557358974c7a2bdbcbe3ccc031b3
ktrant84 <katee.trant@gmail.com>
2016-11-22 10:06:39 -0800

    WIP

--- soda_machine.rb
+++ soda_machine.rb
@@ -11,10 +11,15 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-
+    @sodas.find { |soda| soda == soda_brand}
+    # @sodas.include? soda_brand
+    #   soda_brand
+    # else
+    #   nil
   end
 
   def sell(soda_brand)
+
   end
 
 end
