8bcdceec3a60a3c16626410bd14cc8defd062017
Lisa Nguyen <email": null>
2016-11-22 10:07:07 -0800

    Saving CPC 2

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,34 @@ class SodaMachine
   end
 
   def current_inventory_count
+    p @sodas.count
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |brand|
+      if @sodas == brand
+        p brand.parameterize.underscore.to_sym
+        #puts "#{soda_brand}"
+      else
+        puts "Sorry, the soda you requested is not available."
+        return nil
+      end
+    end
   end
 
   def sell(soda_brand)
+    @sodas.each do |brand|
+      if @sodas != brand
+        puts "Sorry, the soda you requested is not available."
+        return nil
+      else
+        @sodas.has_value?(brand)
+        puts "#{soda_brand}"
+        @cash[@soda] = @price
+        sum = 0
+        @cash.each { |key, value| sum += value.last }
+        @sodas.delete(brand)
+      end
+    end
   end
-
 end
