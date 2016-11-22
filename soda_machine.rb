09a04732c436b09667fafe69899b8c29ad00cb2c
Paul Lee <7.paul.lee@gmail.com>
2016-11-22 10:10:36 -0800

    commit soda machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,28 @@ class SodaMachine
   end
 
   def current_inventory_count
+
+    return @sodas.length
   end
 
   def find_soda(soda_brand)
+    # @sodas.index(soda_brand).inspect
+    return @sodas.find do |soda|
+    soda.brand == soda_brand
+    end
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand)
+      soda = find_soda(soda_brand)
+      @cash -= soda.price
+      @sodas.delete(soda)
+      true
+    else
+      "cannot sell any more soda"
+      false
+    end
+    soda
   end
 
 end
