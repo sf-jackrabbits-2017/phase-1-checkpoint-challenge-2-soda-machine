e35fd4c5190c023da1b72a6f504957f0ce0e5545
rdfm <raulmenchavez@gmail.com>
2016-11-22 10:07:06 -0800

    checkpoint challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,35 @@
+# require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
   def initialize(args = {})
-    @sodas = args[:sodas]
+    @sodas = args.fetch(:sodas){ sodas = [] }
     @cash = args[:cash]
   end
 
   def current_inventory_count
+    @sodas.length 
   end
 
   def find_soda(soda_brand)
+    # self.@sodas.include?(soda_brand)
+    #   @brand
+    # end 
+
+    # soda.rb
+    # pepsi = Soda.new(brand: 'Pepsi', price: 0.65, age: 1)
+    # irb(main):003:0> pepsi.brand 
+    # => "Pepsi"
+    # irb(main):004:0> pepsi.price 
+    # => 0.65
+
+    # soda_machine.rb
+    # soda_machine = SodaMachine.new(sodas: [pepsi, mountain_dew, coke_zero, second_pepsi], cash: 1.00)
   end
 
   def sell(soda_brand)
+
   end
 
 end
