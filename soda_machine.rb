0226636ee15daeac75b64d851b60f79f35c0f119
Cllie Tomlinson <callielynne3@gmail.com>
2016-11-22 09:46:21 -0800

    Work through count inventory and find sodas methods

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    # A soda machine will be able to return the number of sodas it contains. This method should be called SodaMachine#current_inventory_count.
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    # A soda machine will be able to find a single soda with a given brand. This method will be called SodaMachine#find_soda.
+    @sodas.find { |soda| soda.brand == soda_brand }
   end
 
   def sell(soda_brand)
+    # A soda machine will be able to sell a soda with a given brand. When a soda is sold, it should be removed from the soda machine and the price of the soda should be added to the soda machine's cash This method will be called SodaMachine#sell.
+   
   end
 
 end
