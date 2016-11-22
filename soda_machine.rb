4c8ffbec6c5b65d005dcc2b86131ede08377b594
oladejo olakareem <email": null>
2016-11-22 10:11:31 -0800

    Write method for Soda machine

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,28 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.select{|num| num = soda_brand}
   end
 
   def sell(soda_brand)
+
   end
 
 end
+
+# arr_soda =
+# [
+# Soda.new(brand: 'Pepsi', price: 0.65),
+# Soda.new(brand: 'Mountain Dew', price: 0.75),
+# Soda.new(brand: 'Coke Zero', price: 1.00),
+# Soda.new(brand: 'Pepsi', price: 0.65)
+# ]
+
+
+
+# sm = SodaMachine.new(sodas: arr_soda, cash: 1.00)
+# p sm.sodas
