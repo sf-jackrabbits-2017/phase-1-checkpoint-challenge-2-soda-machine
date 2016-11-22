1bedd645bfb3edc84dd406f75194cb437f2a54e9
jyoung488 <jyoung488@gmail.com>
2016-11-22 09:51:50 -0800

    Define find soda method

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -11,9 +13,23 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
+    available_soda = []
+    @sodas.each do |soda|
+      if soda.brand == soda_brand
+        available_soda << soda
+      end
+    end
+    available_soda[0]
   end
 
   def sell(soda_brand)
   end
 
 end
+
+# pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
+# mountain_dew = Soda.new(brand: 'Mountain Dew', price: 0.75)
+#
+# soda_machine = SodaMachine.new(sodas: [pepsi, mountain_dew], cash: 1.00)
+#
+# soda_machine.find_soda('Pepsi')
