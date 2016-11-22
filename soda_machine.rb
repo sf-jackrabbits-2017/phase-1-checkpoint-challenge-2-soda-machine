242fc58bcac073608730c865350af1e3d300b9c4
Sarav Shah <saravsshah@gmail.com>
2016-11-22 10:05:19 -0800

    complete rspec tests

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,28 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.size
   end
 
   def find_soda(soda_brand)
+    #p @sodas
+    #p soda_brand
+    @sodas.find do |soda|
+      soda.brand == soda_brand
+    end
   end
 
   def sell(soda_brand)
+    #p @sodas
+
+    sold_soda = find_soda(soda_brand)
+    #p sold_soda
+    #p soda_brand
+    if !sold_soda.nil?
+      @cash = @cash + sold_soda.price
+      @sodas.delete(sold_soda)
+    else
+    end
   end
 
 end
