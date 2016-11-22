a9a11c4fcb36a3cb2bc2f0d259d6b922977914ee
Derek Pham <derekmpham@gmail.com>
2016-11-22 10:09:01 -0800

    Add current inventory count method

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,17 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    self[@sodas].key(soda_brand) #--> Didn't have time to finish
   end
 
   def sell(soda_brand)
+    @sold_soda = self[:sodas].key(soda_brand) #--> Based on previous method, which I didn't have time to finish
+    purchased_soda_price = self[]
   end
 
 end
+
