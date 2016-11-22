66d59e2b28f57a13df54d1592fea052ef9ee2b3d
Andrew Bruckman <abruckman09@gmail.com>
2016-11-22 09:48:25 -0800

    soda machine sells soda

--- soda_machine.rb
+++ soda_machine.rb
@@ -7,12 +7,23 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.count
   end
 
+
   def find_soda(soda_brand)
+    soda_location = @sodas.find_index{|soda| soda.brand == soda_brand }
+    if soda_location.is_a? Integer
+      @sodas[soda_location]
+    end
   end
 
   def sell(soda_brand)
+    soda = @sodas.find{ |soda| soda.brand == soda_brand}
+    if soda != nil
+      @cash += soda.price
+      @sodas.delete(soda)
+    end
   end
 
 end
