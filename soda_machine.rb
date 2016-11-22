2aebca8f6bb3bda3148a0175adb17642a15fea74
jyoung488 <jyoung488@gmail.com>
2016-11-22 10:05:53 -0800

    Attempt sell method

--- soda_machine.rb
+++ soda_machine.rb
@@ -6,6 +6,7 @@ class SodaMachine
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @sold_soda = []
   end
 
   def current_inventory_count
@@ -23,13 +24,15 @@ class SodaMachine
   end
 
   def sell(soda_brand)
+    if find_soda(soda_brand) == nil
+      nil
+    else
+      @sodas.each do |soda|
+        if soda.brand == soda_brand
+          p soda.brand
+        end
+      end
+    end
   end
 
 end
-
-# pepsi = Soda.new(brand: 'Pepsi', price: 0.65)
-# mountain_dew = Soda.new(brand: 'Mountain Dew', price: 0.75)
-#
-# soda_machine = SodaMachine.new(sodas: [pepsi, mountain_dew], cash: 1.00)
-#
-# soda_machine.find_soda('Pepsi')
