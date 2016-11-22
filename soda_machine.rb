f76f498ed315dec033a274c3aab0b0949eb14700
Katrina DeVaney <katrina.devaney@gmail.com>
2016-11-22 09:34:23 -0800

    Write methods for soda_machine_spec rspecs to pass

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,3 +1,5 @@
+require_relative 'soda'
+
 class SodaMachine
   attr_reader :sodas, :cash
 
@@ -7,12 +9,18 @@ class SodaMachine
   end
 
   def current_inventory_count
+    @sodas.length
   end
 
   def find_soda(soda_brand)
+    @sodas.select{|soda| soda.brand == soda_brand}[0]
   end
 
   def sell(soda_brand)
+    soda = find_soda(soda_brand)
+    return soda if soda == nil
+    @cash += soda.price
+    @sodas.delete(soda)
   end
 
 end
