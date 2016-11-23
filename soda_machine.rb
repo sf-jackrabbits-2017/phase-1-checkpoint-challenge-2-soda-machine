7ed5b58b7b38fe75fbd0056ad2063fac9bbbcbc1
Derek Pham <derekmpham@gmail.com>
2016-11-22 21:57:04 -0800

    Redo find soda method

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,4 +1,4 @@
-class SodaMachine
+class SodaMachine < Soda #--> SodaMachine is under Soda class
   attr_reader :sodas, :cash
 
   def initialize(args = {})
@@ -11,8 +11,13 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-    self[@sodas].key(soda_brand) #--> Didn't have time to finish
-  end
+    @sodas.each do |soda|         # Iterate through each soda instance and
+      if soda.brand == soda_brand #    determine if there's a soda that
+      	return soda 			  #       corresponds with the brand being called for
+      end
+    end
+  	nil # Why do we have to put nil outside of iterator for it to work?
+  end	#    Why can't we return nil in else condition inside each iterator?
 
   def sell(soda_brand)
     @sold_soda = self[:sodas].key(soda_brand) #--> Based on previous method, which I didn't have time to finish
