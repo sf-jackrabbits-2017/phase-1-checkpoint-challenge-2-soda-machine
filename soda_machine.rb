8957f0b80c2b3a43868c87c0c8d53c9188be310b
oladejo olakareem <email": null>
2016-11-26 15:55:42 -0800

    :star: Challenge test pass and :mortar_board:Understand challenge

--- soda_machine.rb
+++ soda_machine.rb
@@ -13,29 +13,19 @@ class SodaMachine
   end
 
   def find_soda(soda_brand)
-    @sodas.find{|num| num.brand == soda_brand}
+    @sodas.find{|soda| soda.brand == soda_brand}
+
   end
 
   def sell(soda_brand)
-    if find_soda(soda_brand)
-      find_soda(soda_brand)
-      @cash = @cash + soda.price
-      @soda.delete(soda)
+    soda = find_soda(soda_brand)
+    if soda
+      @cash += soda.price
+      @sodas.delete(soda)
     else
-      "no soda available!"
       nil
+    end
   end
 end
 
-# arr_soda =
-# [
-# Soda.new(brand: 'Pepsi', price: 0.65),
-# Soda.new(brand: 'Mountain Dew', price: 0.75),
-# Soda.new(brand: 'Coke Zero', price: 1.00),
-# Soda.new(brand: 'Pepsi', price: 0.65)
-# ]
 
-
-
-# sm = SodaMachine.new(sodas: arr_soda, cash: 1.00)
-# p sm.sodas
